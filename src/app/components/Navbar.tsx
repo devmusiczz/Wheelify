989c13f8a27969d80ee0a5e032a224b58a5774f9
devmusiczz <onlyfordance0@gmail.com>
2024-06-22 22:38:52 +0530

    Added Responsive Featured Product List

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import NavIcons from './NavIcons';
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-18 xl:px-32 2xl:px-64 relative">
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       <div className='h-full flex items-center justify-between md:hidden'>
         {/* Mobile */}
         <Link href="/">
