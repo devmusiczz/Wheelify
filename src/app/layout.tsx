59fd289187b3a015f51acab18662a81843bad55a
devmusiczz <onlyfordance0@gmail.com>
2024-07-01 21:32:18 +0530

    setup the backend with wix studio and fetched the product list from backend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { WixClientContextProvider } from "@/context/wixContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,13 +16,15 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) { 
+}>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
+        <WixClientContextProvider>
+          <Navbar/>
+          {children}
+          <Footer/>
+        </WixClientContextProvider>
         </body>
     </html>
   );
