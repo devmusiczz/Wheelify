eff9652f224631014a809e52861654961ebe55fc
devmusiczz <devr69080@gmail.com>
2024-07-23 13:00:46 +0530

    added profile page, reviews feature

--- src/lib/wixClientServer.ts
+++ src/lib/wixClientServer.ts
@@ -1,32 +1,32 @@
-import { OAuthStrategy, createClient } from '@wix/sdk';
-import { products, collections } from '@wix/stores';
-import {cookies} from 'next/headers'
+import { OAuthStrategy, createClient } from "@wix/sdk";
+import { collections, products } from "@wix/stores";
+import { orders } from "@wix/ecom";
+import { cookies } from "next/headers";
+import { members } from '@wix/members';
 
 export const wixClientServer = async () => {
-    
-let refreshToken;
+  let refreshToken;
 
-try{
-const cookieStore = cookies();
- refreshToken = JSON.parse( cookieStore.get('refreshToken')?.value || '{}') ;
-} catch (e) {}
+  try {
+    const cookieStore = cookies();
+    refreshToken = JSON.parse(cookieStore.get("refreshToken")?.value || "{}");
+  } catch (e) {}
 
- const wixClient = createClient({
+  const wixClient = createClient({
     modules: {
-        products,
-        collections,
+      products,
+      collections,
+      orders,
+      members,
     },
     auth: OAuthStrategy({
-        clientId : process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
-        tokens: {
-            refreshToken,
-            accessToken: {
-                value: "" , expiresAt: 0
-            },
-        }
+      clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+      tokens: {
+        refreshToken,
+        accessToken: { value: "", expiresAt: 0 },
+      },
     }),
-});
-    
-    return wixClient
+  });
 
-}
+  return wixClient;
+};
