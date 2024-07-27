4f12bfab90517cdf685ca3bbfcf4a15d3978106f
devmusiczz <devr69080@gmail.com>
2024-07-28 02:45:10 +0530

    sticky navbar at top and navicons visible on mobile devices too

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,19 @@
 import Link from "next/link";
-import Menu from "./Menu";
 import Image from "next/image";
 import SearchBar from "./SearchBar";
 import dynamic from "next/dynamic";
-// import NavIcons from "./NavIcons";
 
 const NavIcons = dynamic(() => import("./NavIcons"), { ssr: false });
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+    <div className="navbar h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 fixed top-0 left-0 w-full z-50">
       {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
           <div className="text-2xl tracking-wide">AUTOSWAGS</div>
         </Link>
-        <Menu />
+        <NavIcons />
       </div>
       {/* BIGGER SCREENS */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
@@ -28,7 +26,6 @@ const Navbar = () => {
           <div className="hidden xl:flex gap-4">
             <Link href="/">Homepage</Link>
             <Link href="/">Shop</Link>
-            {/* <Link href="/">Deals</Link> */}
             <Link href="/">About</Link>
             <Link href="/">Contact</Link>
           </div>
