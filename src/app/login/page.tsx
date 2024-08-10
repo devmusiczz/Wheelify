ae0d56017c024b08322d81d32b2525a8c7fc625e
devmusiczz <devr69080@gmail.com>
2024-08-11 01:16:30 +0530

    fix log in ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -131,7 +131,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="h-[calc(100vh-80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
+    <div className="h-[calc(100vh-80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex mt-24 items-center justify-center">
       <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
         <h1 className="text-2xl font-semibold">{formTitle}</h1>
         {mode === MODE.REGISTER ? (
@@ -171,7 +171,7 @@ const LoginPage = () => {
         )}
         {mode === MODE.LOGIN || mode === MODE.REGISTER ? (
           <div className="flex flex-col gap-2">
-            <label className="text-sm bg-zinc-900 text-gray-100">Password</label>
+            <label className="text-sm text-gray-100">Password</label>
             <input
               type="password"
               name="password"
